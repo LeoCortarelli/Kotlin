870d136a0118eb789306b8b217057e50d8384d3e
Leonardo Tavares Cortarelli <105895556+LeoCortarelli@users.noreply.github.com>
2023-11-24 18:02:20 -0300

    Delete exercicio2.kt

--- exercicio2.kt
+++
@@ -1,9 +0,0 @@
-fun main() {
-    val numero = 5
-    
-    val antecessor = numero - 1
-    val sucessor = numero + 1
-    
-    println("O antecessor de $numero é $antecessor")
-    println("O sucessor de $numero é $sucessor")
-}
